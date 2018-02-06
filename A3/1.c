621bd6ec0fec70ed40b68868b565371a4ebf898f
Vishal <vishalcross36@gmail.com>
2018-02-07 00:00:29 +0530

    Updated1

--- A3/1.c
+++ A3/1.c
@@ -1,28 +1,78 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int height[n],price[n],cost1=0,cost2=0;
-    for(int i=0;i<n;i++) scanf("%d",&height[i]);
-    for(int i=0;i<n;i++) scanf("%d",&price[i]);
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+long long int cop(int n,long long int* c,long long int * a,long long int val){
+	long long int sum=0;
+	int p;
+	for(p=0;p<n;p++){
+		sum=sum+abs(a[p]-val)*c[p];
+	}
+	return sum;
 
-    double avg=0,sum=0,sum2=0;
-    for(int i=0;i<n;i++){
-      sum += ((height[i])*(price[i]) );
-      sum2 += price[i];
-    }
-    avg = (double)sum/sum2;
-    printf("%lf\n", avg);
-    for(int i=0;i<n;i++){
-      cost1 += (int) (abs(height[i] - (floor)(avg)) * price[i]);
-    }
-    for(int i=0;i<n;i++){
-      cost2 += (int) (abs(height[i] - (ceil)(avg)) * price[i]);
-    }
-    printf("%d %d\n",cost1,cost2);
-    if(cost1<=cost2) printf("Min cost is %d",cost1);
-    else printf("Min cost is %d",cost2);
-    return 0;
+
+}
+
+
+void main(){
+
+	int n,i,j;
+	long long high,low=0;
+	high=LLONG_MIN;
+	scanf("%d",&n);
+
+	long long int a[n],c[n];
+
+	for(i=0;i<n;i++){
+		scanf("%lld",&a[i]);
+		if(a[i]>high){
+			high=a[i];
+		}
+	}
+	for(i=0;i<n;i++){
+		scanf("%lld",&c[i]);
+	}
+
+	high = high+1;
+	long long int mid,ans;
+	ans=LLONG_MAX;
+	long long int bm=0,mm=0,am=0;
+	while(high>low){
+		mid = (high+low)>>1;
+
+		if(mid>0){
+			bm=cop(n,c,a,mid-1);
+
+		}
+		else{
+			bm=LLONG_MAX;
+		}
+		mm=cop(n,c,a,mid);
+		am=cop(n,c,a,mid+1);
+
+		printf("mid is --%lld and bm is %lld and mm is %lld and am is %lld\n",mid,bm,mm,am );
+		printf("high is %lld and low is  %lld\n",high,low );
+		if(ans==mm||am==bm){
+            ans=mm;
+			break;
+		}
+
+		if(ans>mm){
+			ans=mm;
+		}
+
+		if(bm<=mm){
+			high = mid;
+		}
+		else if(am<=mm){
+			low = mid;
+		}
+
+
+
+
+	}
+	printf("%lld\n",ans );
+	printf("%lld\n",mid );
+
+
 }
